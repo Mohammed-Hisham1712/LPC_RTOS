4db02fa34654a83a27e498ae638f88690a0ceef2
Mohamed Hisham <mhisham@brightskiesinc.local>
2023-11-29 09:26:54 +0200

    Add IPC task1 files

---
+++ IPC_Task1/IPC_task1.h
@@ -0,0 +1,15 @@
+#ifndef __IPC_TASK1_H__
+
+#define __IPC_TASK1_H__
+
+#include "FreeRTOS.h"
+
+/**
+ * @brief Creates the two tasks running on the system.
+ * 
+ * @return pdPASS on sueccess
+ */
+BaseType_t IPC_task1_init(void);
+
+
+#endif
